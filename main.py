12af6aac43395a90295c409f853669db6a3bf7f1
SpeedsKillsx <amayaslabchri88@gmail.com>
2023-05-13 20:40:47 +0100

    main

--- main.py
+++ main.py
@@ -9,16 +9,51 @@ from matplotlib.figure import Figure
 from matplotlib.gridspec import GridSpec
 import numpy as np
 from PIL import Image
-
-
+from Preporcessing import *
 class MainWindow(QMainWindow):
-    
+    def LoadModels(self):
+        path_modelX = "Models/ModelX/capsule-5"
+        path_modelY = "Models/Model_y_92/capsule-5"
+        path_modelZ = "Models/ModelZ/capsule-5"
+        #! loadX
+        self.ModelX = CapsuleNetwork(**params)
+        Xcheckpoint = tf.train.Checkpoint(model=self.ModelX)
+        Xcheckpoint.restore(path_modelX)
+        #! loadY
+        self.ModelY = CapsuleNetwork(**params)
+        Ycheckpoint = tf.train.Checkpoint(model=self.ModelY)
+        Ycheckpoint.restore(path_modelY)
+        #! loadZ
+        self.ModelZ = CapsuleNetwork(**params)
+        Zcheckpoint = tf.train.Checkpoint(model=self.ModelZ)
+        Zcheckpoint.restore(path_modelZ)
+        
     def StartClassification(self):
-        pass
+        self.LoadModels()
+        classification = final_pred(self.Nodule3D[4,0][:,:,32], self.Nodule3D[4,0][:,32,:],self.Nodule3D[4,0][32,:,:],self.ModelX,self.ModelY,self.ModelZ)
+        result = None
+        if classification == 0:
+            result="benin"
+        else:
+            result="maliscious"
+        print(result)
+        
+        ##! Classification de X
+        pr = predictP(self.ModelX,tf.expand_dims(Preprocessing(self.Nodule3D[4,0][:,:,32]),0))
+        self.ui.label_X_2.setText(str(np.max(pr))+" %")
+        
+        prY = predictP(self.ModelY,tf.expand_dims(Preprocessing(self.Nodule3D[4,0][:,32,:]),0))
+        self.ui.label_Y_4.setText(str(np.max(prY))+" %")
+        
+        prZ = predictP(self.ModelZ,tf.expand_dims(Preprocessing(self.Nodule3D[4,0][32,:,:]),0))
+        self.ui.label_Z_5.setText(str(np.max(prZ))+" %")
+        self.ui.label_12.setText("Final Classification: "+result)
+        
+        
     def clicker(self):
         print("You clicked the button to choose a file !")
-        fname = QFileDialog.getOpenFileName(self, "Open File and Choose the 3D Nodule", "C:\\Users\\21379\\Desktop\\memoire\\DATA" , "All Files (*);; Python Files(*.npy)")
-        Nodule3D = np.load(fname[0], allow_pickle=True)
+        fname = QFileDialog.getOpenFileName(self, "Open File and Choose the 3D Nodule", "" , "All Files (*);; Python Files(*.npy)")
+        self.Nodule3D = np.load(fname[0], allow_pickle=True)
         #print(Nodule3D.shape)   
         #plt.plot(Nodule3D[10,:,:,0])
         #plt.show()
@@ -30,13 +65,13 @@ class MainWindow(QMainWindow):
         #im.show()
         #plt.imshow(Nodule3D[10,:,:,0],'gray')
         self.ax1 = self.figure.add_subplot(self.grid[0, 0])
-        self.ax1.imshow(Nodule3D[0,0][32, :, :], cmap='gray')
+        self.ax1.imshow(self.Nodule3D[4,0][32, :, :], cmap='gray')
         self.ax1.set_title('Vue X')
         self.ax2 = self.figure.add_subplot(self.grid[0, 1])
-        self.ax2.imshow(Nodule3D[0,0][:, 32, :], cmap='gray')
+        self.ax2.imshow(self.Nodule3D[4,0][:, 32, :], cmap='gray')
         self.ax2.set_title('Vue Y')
         self.ax3 = self.figure.add_subplot(self.grid[0, 2])
-        self.ax3.imshow(Nodule3D[0,0][:, :, 32], cmap='gray')
+        self.ax3.imshow(self.Nodule3D[4,0][:, :, 32], cmap='gray')
         self.ax3.set_title('Vue Z')
         #refresh
         self.canvas.draw()
@@ -104,8 +139,13 @@ class MainWindow(QMainWindow):
         #close Notification 
         self.ui.closeNotificationBtn.clicked.connect(lambda: self.ui.popupNotificationContainer.collapseMenu())
         
+        #close Notification 
+        self.ui.closeNotificationBtn.clicked.connect(lambda: self.ui.popupNotificationContainer.collapseMenu())
         
-        
+        # Classification
+        self.ui.startClassificationBtn.clicked.connect(self.StartClassification)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     
